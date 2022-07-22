da567542914e2025e23a8a0c07fcf500751b21e3
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-07-22 13:23:23 -0600

    Thread sleep

--- ConcurringProgramming/src/concurrency/MyThread.java
+++ ConcurringProgramming/src/concurrency/MyThread.java
@@ -1,13 +1,24 @@
 package concurrency;
 
 public class MyThread implements Runnable {
+    public MyThread() {
+    }
+
     @Override
     public void run() {
-    System.out.println("Este bloque se ejecuto en un hilo");
+    for (int i = 0; i <5; i++ ){
+        try {
+            Thread.sleep(3500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("i: "+i);
+    }
     }
 
   public static void main(String[] args) {
-    Thread myThread = new Thread (new MyThread());
-    myThread.start();
+
+        Thread myThread = new Thread(new MyThread());
+        myThread.start();
   }
 }
