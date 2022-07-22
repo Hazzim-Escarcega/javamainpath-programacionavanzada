493071ed4b56238e7f1d73a8b70460a296c8a131
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-07-22 13:44:23 -0600

    Running multiple threads / thread naming

--- ConcurringProgramming/src/concurrency/MyThread.java
+++ ConcurringProgramming/src/concurrency/MyThread.java
@@ -6,19 +6,25 @@ public class MyThread implements Runnable {
 
     @Override
     public void run() {
-    for (int i = 0; i <5; i++ ){
+    for (int i = 1; i <5; i++ ){
         try {
             Thread.sleep(3500);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("i: "+i);
+        System.out.println("i: "+i+" Thread ID "+Thread.currentThread().getName());
+
     }
     }
 
   public static void main(String[] args) {
 
-        Thread myThread = new Thread(new MyThread());
-        myThread.start();
+        Thread firstThread = new Thread(new MyThread(), "FirstThread");
+        Thread secondThread = new Thread(new MyThread(), "SecondThread");
+
+        secondThread.setName("2nd Thread");
+        firstThread.start();
+        secondThread.start();
+
   }
 }
