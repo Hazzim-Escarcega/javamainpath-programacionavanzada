e7c7f06dc1bf4d8ea3dacc550087d4e3ceaafc97
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-07-25 09:20:10 -0600

    Thread states

--- ConcurringProgramming/src/concurrency/ThreadLifeCycle.java
+++ ConcurringProgramming/src/concurrency/ThreadLifeCycle.java
@@ -7,11 +7,11 @@ public class ThreadLifeCycle {
         public void run() {
             for (int i = 1; i < 5; i++){
                 try{
-                    Thread.sleep(1000);
+                    Thread.sleep(3000);
                 }catch (InterruptedException e ){
                     e.printStackTrace();
                 }
-                System.out.println("I'm walking "+Thread.currentThread().getName());
+                System.out.println("Action: I'm walking ThreadName: "+Thread.currentThread().getName()+" Thread state: "+Thread.currentThread().getState());
             }
         }
     }
@@ -21,22 +21,33 @@ public class ThreadLifeCycle {
         public void run() {
             for (int i = 1; i < 5; i++){
                 try{
-                    Thread.sleep(1000);
+                    Thread.sleep(3000);
                 }catch (InterruptedException e ){
                     e.printStackTrace();
                 }
-                System.out.println("I'm chewing gum "+Thread.currentThread().getName());
+                System.out.println("I'm chewing gum "+Thread.currentThread().getName()+" Thread state: "+Thread.currentThread().getState());
             }
         }
     }
         public static void main(String[] args){
             Thread walkThread = new Thread(new Walk(), "Walk thread");
             Thread gumThread = new Thread(new ChewGum(),"Chew thread");
+            System.out.println("Walk Thread state: "+walkThread.getState());
+            System.out.println("Main Thread state: "+Thread.currentThread().getState());
 
             try{
                 walkThread.start();
-                walkThread.join(1000);
-                gumThread.start();
+                System.out.println("Walk Thread state: "+walkThread.getState());
+                System.out.println("Main Thread state: "+Thread.currentThread().getState());
+
+                Thread.sleep(1000);
+                walkThread.join(5000);
+                System.out.println("Walk Thread state after join: "+walkThread.getState());
+                System.out.println("Main Thread state after join: "+Thread.currentThread().getState());
+                System.out.println("Main thread will sleep for 20s");
+                Thread.sleep(20000);
+                System.out.println("Walk Thread state: "+walkThread.getState());
+                System.out.println("Main Thread state: "+Thread.currentThread().getState());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
