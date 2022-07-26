8197b983562dd53edff3c6e93023dfc0bfb2abbb
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-07-26 13:46:18 -0600

    REST API MS SQL JPA SPRING BOOT

---
+++ restapimssql/src/main/java/atos/upgrade/restapimssql/model/Employee.java
@@ -0,0 +1,63 @@
+package atos.upgrade.restapimssql.model;
+
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailID;
+
+    public Employee() {
+    }
+
+    public Employee(long id, String firstName, String lastName, String emailID) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailID = emailID;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "first_name", nullable = false)
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name", nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "email_address", nullable = false)
+    public String getEmailId() {
+        return emailID;
+    }
+    public void setEmailId(String emailID) {
+        this.emailID = emailID;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailID
+                + "]";
+    }
+}
