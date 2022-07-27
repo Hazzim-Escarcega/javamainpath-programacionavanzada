dbebefa47d39cc880041c16df1142946ec73e240
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-07-27 16:02:09 -0600

    JPA Relations many to one, one to many

---
+++ jparelations/src/test/java/atos/upgrade/jparelations/JparelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package atos.upgrade.jparelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JparelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
